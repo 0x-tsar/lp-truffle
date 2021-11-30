a3b440ffb4d38cf12abd8f297e61bc28991da2a1
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 02:44:33 -0300

    :sparkles:

--- contracts/Operator.sol
+++ contracts/Operator.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Operator {
-    constructor() {}
+    IERC20 public liquidityToken;
+
+    constructor(address lpAddress) {
+        liquidityToken = IERC20(lpAddress);
+    }
+
+    function name(type name) {
+      
+    }
 }
