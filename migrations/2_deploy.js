61c960c85854c9b380f544cf603b8f16a49d9823
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 02:58:55 -0300

    :sparkles:

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,13 +1,4 @@
 const LiquidityProvider = artifacts.require("LiquidityProvider");
 const Operator = artifacts.require("Operator");
 
-module.exports = async () => {
-  try {
-    const liquidityProvider = await LiquidityProvider.deployed();
-    const operator = await Operator.deployed();
-
-    //
-  } catch (error) {
-    console.log(error);
-  }
-};
+module.exports = async (deployer, network, accounts) => {};
