f6b4bf6961262a6939b3223ad4ce0415b77efa57
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 04:32:07 -0300

    :sparkles:

--- contracts/Operator.sol
+++ contracts/Operator.sol
@@ -6,7 +6,6 @@ import "./LiquidityProvider.sol";
 
 contract Operator is ERC20 {
     LiquidityProvider public liquidityToken;
-    uint256 public FEE = 1_000; //10%
 
     constructor(address lpAddress) ERC20("Operator Token", "OPT") {
         liquidityToken = LiquidityProvider(lpAddress);
@@ -15,9 +14,7 @@ contract Operator is ERC20 {
     function deposit(uint256 amount) public {
         //aprove this contract first first
         liquidityToken.transferFrom(msg.sender, address(this), amount);
-        //amount putted + 10% of amount
-        uint256 lp = calculateFee(amount);
-        liquidityToken.mint(msg.sender, amount + lp);
+        liquidityToken.mint(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) public {
@@ -25,9 +22,9 @@ contract Operator is ERC20 {
         liquidityToken.transfer(msg.sender, amount);
     }
 
-    //185 basis points = 1.85 pct
-    function calculateFee(uint256 amount) public view returns (uint256) {
-        require((amount / 10000) * 10000 == amount, "too small");
-        return (amount * FEE) / 10000;
-    }
+    // //185 basis points = 1.85 pct
+    // function calculateFee(uint256 amount) public view returns (uint256) {
+    //     require((amount / 10000) * 10000 == amount, "too small");
+    //     return (amount * FEE) / 10000;
+    // }
 }
