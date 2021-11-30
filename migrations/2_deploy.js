f9c7bfef8d36893c63c87461805d35fd55175511
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 05:25:26 -0300

    :sparkles:

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,9 +1,13 @@
 const LiquidityProvider = artifacts.require("LiquidityProvider");
 const Operator = artifacts.require("Operator");
+const Dai = artifacts.require("Dai");
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(LiquidityProvider);
   const liquidityProvider = await LiquidityProvider.deployed();
+
+  await deployer.deploy(Dai);
+  const dai = await Dai.deployed();
 
   await deployer.deploy(Operator, liquidityProvider.address);
   const operator = await Operator.deployed();
